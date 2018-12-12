d13281f18b941e3315227233fc0b3d164fcf9ab2
Kria <krianoto@gmail.com>
2018-12-12 22:58:29 +1030

    Project Upload

---
+++ Skip (Cross Platform)/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody        m_rb;
+    private SphereCollider   m_col;
+    private AudioSource      m_audio;
+
+    public LayerMask        m_groundLayers;
+
+    public AudioClip        m_jumpSound;
+    public AudioClip        m_hitSound;
+    public GameObject       m_rope;
+
+    void Start ()
+    {
+        m_rb = GetComponent<Rigidbody>();
+        m_col = GetComponent<SphereCollider>();
+        m_audio = GetComponent<AudioSource>();
+    }
+
+    void Update ()
+    {
+        bool SpacePressed = Input.GetKey(KeyCode.Space);
+        bool ScreenTapped;
+
+        if (Input.touchCount > 0)
+            ScreenTapped = true;
+        else
+            ScreenTapped = false;
+
+
+        if (IsGrounded() == true && (SpacePressed || ScreenTapped))
+        {
+            m_rb.AddForce(Vector3.up * 5, ForceMode.Impulse);           //Jump and play jump sound if input is either spacebar or a screen tap.
+            m_audio.PlayOneShot(m_jumpSound);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == m_rope.tag)            //Play hit sound if the player is hit by the rope, reset rope rotation speed.
+        {
+            m_audio.PlayOneShot(m_hitSound);
+            RotationScript ropeRot = other.GetComponentInParent<RotationScript>();
+            ropeRot.m_angle = ropeRot.m_minSpeed;
+        }
+    }
+
+    //Checks if the player sphere is touching the ground. Returns bool.
+    private bool IsGrounded()
+    {
+        return Physics.CheckSphere(m_col.bounds.center, m_col.radius * 1.2f, m_groundLayers);
+    }
+}
---
+++ Skip (Cross Platform)/Assets/Scripts/RotationScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class RotationScript : MonoBehaviour
+{
+    public float    m_angle;
+    public float    m_minSpeed = 50;
+    private float   m_lastTime = 0f;
+
+	void Start ()
+    {
+        m_angle = m_minSpeed;
+	}
+	
+	void Update ()
+    {
+        if (Time.fixedTime > (m_lastTime + 4) && m_angle < (m_minSpeed * m_minSpeed))   //Add an amount from 20 to 40 to m_angle every 4 seconds, capped at m_minSpeed squared.
+        {
+            m_angle += Random.Range(20f, 40f);
+            m_lastTime = Time.fixedTime;
+        }
+
+        transform.Rotate(Vector3.forward, m_angle * Time.deltaTime);                    //Simply rotates the "Skipping Rope"
+	}
+}
